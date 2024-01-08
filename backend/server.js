e5b6d4364ba07b1caaf774c8cf2fd6c7a09560e9
Anas NadY <anasabdallahnady@gmail.com>
2024-01-08 16:56:34 +0200

    change backent port

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ mongoose
   .connect(process.env.MONGO_DB_URL)
   .then(() => console.log("DB connection successful!"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8800;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
