f4681b35573c144f215bec43e211a7cee16c5578
Anas Abdallah Nady <eng.anas.nady@gmail.com>
2024-08-22 14:47:58 +0300

    Update `getSurahInfo` to include `nextSurah` and `previousSurah` data in the response.

--- backend/controllers/surahController.js
+++ backend/controllers/surahController.js
@@ -6,17 +6,31 @@ const Recitation = require("./../models/recitationModel.js");
 
 exports.getSurahInfo = asyncHandler(async (req, res, next) => {
   const slug = req.params.slug;
+  const fields = `arabicName englishName slug number ${req.query.selectField}`;
 
-  const surah = await Surah.findOne({ slug }).select(
-    "arabicName englishName slug number"
-  );
+  const surah = await Surah.findOne({ slug }).select(fields);
   if (!surah) {
     return next(new AppError(`Surah: ${slug} is not found`, 404));
+  }
+
+  const surahNumber = parseInt(surah.number);
+  let previousSurah;
+  let nextSurah;
+
+  if (surahNumber > 1) {
+    previousSurah = await Surah.findOne({ number: surahNumber - 1 }).select(
+      fields
+    );
+  }
+  if (surahNumber < 144) {
+    nextSurah = await Surah.findOne({ number: surahNumber + 1 }).select(fields);
   }
 
   res.status(200).json({
     status: "success",
     surah,
+    previousSurah,
+    nextSurah,
   });
 });
 
