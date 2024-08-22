bf9b57397bdac8e030f89b6aa41505d03e574d03
Anas Abdallah Nady <eng.anas.nady@gmail.com>
2024-08-22 14:51:36 +0300

    Add `quranicSurahs link` to Footer component

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -6,9 +6,15 @@ function Footer({ currentLang }) {
   const t = useTranslations("Footer");
   const currentYear = new Date().getFullYear();
 
+  const links = [
+    { href: `/${currentLang}/surah`, title: t("quranicSurahs") },
+    { href: `/${currentLang}/about-us`, title: t("aboutTitle") },
+    { href: `/${currentLang}/contact-us`, title: t("contactTitle") },
+  ];
+
   return (
     <footer className="bg-slate-50 dark:bg-gray-900 h-[185px] border-t-[1px] border-gray-200 dark:border-gray-700 py-6">
-      <div className="container w-full p-4">
+      <div className="container w-full py-4">
         <div className="flex flex-col flex-wrap items-center justify-center gap-5 mb-4 md:flex-row md:justify-between sm:mb-0">
           <Link
             href="/"
@@ -16,16 +22,14 @@ function Footer({ currentLang }) {
           >
             <SiteTitle />
           </Link>
-          <ul className="flex flex-wrap items-center font-medium text-orange-500 gap-9 text-[18px] sm:text-xl lg:text-2xl dark:text-orange-500">
-            <Link href={`/${currentLang}/about-us`} className="hover:underline">
-              {t("aboutTitle")}
-            </Link>
-            <Link
-              href={`/${currentLang}/contact-us`}
-              className="hover:underline"
-            >
-              {t("contactTitle")}
-            </Link>
+          <ul className="flex flex-wrap sm:items-center font-medium text-orange-500 gap-4 sm:gap-9 ltr:text-sm rtl:text-[18px] rtl:sm:text-xl rtl:lg:text-2xl ltr:sm:text-xl ltr:lg:text-2xl dark:text-orange-500">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link href={link.href} className="hover:underline">
+                  {link.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="flex items-center justify-center gap-2 pt-2 mt-3 border-t-[1px] border-gray-200 md:mt-5 dark:border-gray-700">
