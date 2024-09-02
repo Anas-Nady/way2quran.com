4d5c990aeeba7e4ad9ca2abf5b4e094599c70e3c
Anas Nady <eng.anas.nady@gmail.com>
2024-09-02 08:02:44 +0300

    feat: Add `sendMessage` controller for handling email sending

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,6 +1,9 @@
 const messageModel = require("./../models/messageModel.js");
 const AppError = require("./../utils/appError.js");
 const asyncHandler = require("express-async-handler");
+const smtpTransporter = require("../utils/smtpTransport.js");
+const EmailTemplate = require("../templates/email.js");
+const { SUBJECT } = require("../constants/email.js");
 
 exports.listAllMessages = asyncHandler(async (req, res, next) => {
   const pageSize = Number(req.query.pageSize) || 6;
@@ -67,6 +70,30 @@ exports.createMessage = asyncHandler(async (req, res, next) => {
     message: "success",
     data: newMessage,
   });
+});
+
+exports.sendMessage = asyncHandler(async (req, res, next) => {
+  const { receiverEmail, content } = req.body;
+
+  if (!receiverEmail || !content) {
+    return next(new AppError("Please provide all data for us", 400));
+  }
+
+  try {
+    const mailOptions = {
+      from: `Way2quran.com ${process.env.SMTP_EMAIL}`,
+      to: receiverEmail,
+      subject: SUBJECT,
+      html: EmailTemplate({ content }),
+    };
+
+    // Send email using SMTP transporter
+    await smtpTransporter.sendMail(mailOptions);
+  } catch (error) {
+    return next(new AppError("Failed to send email ", error, 500));
+  }
+
+  res.status(200).json({ message: "success" });
 });
 
 exports.deleteMessage = asyncHandler(async (req, res, next) => {
