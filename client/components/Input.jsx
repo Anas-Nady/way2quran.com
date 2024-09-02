f7e7ed5486549895a39c1602bac279f52b6b8677
Anas Nady <eng.anas.nady@gmail.com>
2024-09-02 08:44:46 +0300

    style: Update `Input` component text size for difference screen width

--- client/components/Input.jsx
+++ client/components/Input.jsx
@@ -17,7 +17,7 @@ const Input = ({
     <div className="mb-5">
       <label
         htmlFor={id}
-        className={`block mb-2 capitalize text-lg ${
+        className={`block mb-2 capitalize text-lg sm:text-xl font-medium text-center ${
           isAdmin && "text-xl"
         } font-medium text-gray-900 dark:text-white`}
       >
@@ -28,7 +28,7 @@ const Input = ({
         id={id}
         value={value}
         onChange={onChange}
-        className={`${extraClasses} font-english max-w-full bg-gray-50 border border-gray-300 text-gray-900 text-lg sm:text-xl rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500`}
+        className={`${extraClasses} font-english max-w-full bg-gray-50 border border-gray-300 text-gray-900 text-lg sm:text-xl rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full py-1.5 px-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500`}
         placeholder={placeholder}
         required={required}
         disabled={disabled}
