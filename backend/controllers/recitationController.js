fc2a41c4a1548904973a4444c6114c0e4dc6928c
Anas Nady <eng.anas.nady@gmail.com>
2024-09-04 23:43:11 +0300

    Fix: Remove `totalListeners` tracking from getRecitation handler

--- backend/controllers/recitationController.js
+++ backend/controllers/recitationController.js
@@ -1,7 +1,6 @@
 const Recitation = require("../models/recitationModel");
 const asyncHandler = require("express-async-handler");
 const AppError = require("../utils/appError");
-const ViewRecord = require("../models/viewRecordModel");
 
 exports.getAllRecitations = asyncHandler(async (req, res, next) => {
   const recitations = await Recitation.find({});
@@ -21,29 +20,6 @@ exports.getRecitation = asyncHandler(async (req, res, next) => {
 
   if (!recitation) {
     return next(new AppError("The recitation not found", 404));
-  }
-
-  const userIp =
-    req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-  // Check if this IP has already been counted
-  const existingView = await ViewRecord.findOne({
-    "reference.type": "Recitation",
-    "reference.id": recitation._id,
-    userIp,
-  });
-
-  if (!existingView) {
-    recitation.totalListeners++;
-    await recitation.save();
-
-    await ViewRecord.create({
-      reference: {
-        type: "Recitation",
-        id: recitation._id,
-      },
-      userIp,
-    });
   }
 
   res.status(200).json({
