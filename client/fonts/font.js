b7f601ed668de8d58e9c4722cc0f5fadb07ba51d
Anas Nady <eng.anas.nady@gmail.com>
2024-09-04 22:58:34 +0300

    Enhance font loading by adding `display`, `fallback`, and `preload` options for arabicFont, englishFont, and ayatQuranFont

--- client/fonts/font.js
+++ client/fonts/font.js
@@ -4,13 +4,23 @@ import { Noto_Naskh_Arabic, Public_Sans } from "next/font/google";
 const arabicFont = Noto_Naskh_Arabic({
   subsets: ["arabic"],
   weight: ["400", "500", "600", "700"],
+  display: "swap",
+  fallback: ["Arial", "sans-serif"],
+  preload: true,
 });
 
 const englishFont = Public_Sans({
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800"],
+  display: "swap",
+  fallback: ["system-ui", "sans-serif"],
+  preload: true,
 });
 
-const ayatQuranFont = localFont({ src: "./AyatQuran.ttf" });
+const ayatQuranFont = localFont({
+  src: "./AyatQuran.ttf",
+  display: "swap",
+  preload: true,
+});
 
 export { arabicFont, englishFont, ayatQuranFont };
