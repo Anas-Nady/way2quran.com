3b94262381967cd900ed8778e28bd57033795270
Anas Nady <eng.anas.nady@gmail.com>
2024-09-06 09:33:42 +0300

    style: Add border style to IconButton component

--- client/components/HeaderSection/IconButton.jsx
+++ client/components/HeaderSection/IconButton.jsx
@@ -14,7 +14,7 @@ const IconButton = ({
       type={type}
       aria-label={ariaLabel}
       onClick={onClick}
-      className={`px-4 py-2 text-sm font-medium text-center text-black rounded-lg bg-slate-100 hover:bg-slate-200 focus:ring-4 focus:outline-none dark:focus:outline-none lg:px-4 dark:text-white dark:bg-slate-600 dark:hover:bg-slate-700 ${className}`}
+      className={`px-4 py-2 text-sm font-medium text-center text-black rounded-lg bg-slate-200 hover:bg-slate-300 border border-gray-300 dark:border-gray-500 focus:ring-4 focus:outline-none dark:focus:outline-none lg:px-4 dark:text-white dark:bg-slate-600 dark:hover:bg-slate-700 ${className}`}
       {...props}
     >
       {children}
