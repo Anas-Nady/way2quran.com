7d2cebb2ddfd9041758dcf469f79f1f06eebd3f7
Anas Nady <eng.anas.nady@gmail.com>
2024-09-27 22:53:05 +0300

    Fix URL patterns for reciters, recitations, and surahs in `SearchResults` component

--- client/src/components/Header/Search/SearchResults.tsx
+++ client/src/components/Header/Search/SearchResults.tsx
@@ -31,28 +31,38 @@ const SearchResults: React.FC<SearchResultsProps> = ({
     toggleSearchPopup();
   };
 
-  const renderResults = (items: Result[], pathPrefix: string) =>
-    items.map((result, index) => (
-      <li
-        key={index}
-        onClick={() =>
-          handleNavigation(`/${locale}/${pathPrefix}/${result.slug}`)
-        }
-        className="block px-2 py-3 font-medium text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 text-start text-md sm:text-lg lg:text-xl"
-        role="button"
-        tabIndex={0}
-      >
-        {getName(result, locale)}
-      </li>
-    ));
+  const renderResults = (items: Result[], type: string) =>
+    items.map((result, index) => {
+      let url = `/${locale}`;
+
+      if (type === "reciter") {
+        url += `/reciters/${result.slug}`;
+      } else if (type === "recitation") {
+        url += `/reciters?recitationSlug=${result.slug}`;
+      } else if (type === "surah") {
+        url += `/surahs/${result.slug}`;
+      }
+
+      return (
+        <li
+          key={index}
+          onClick={() => handleNavigation(url)}
+          className="block px-2 py-3 font-medium text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 text-start text-md sm:text-lg lg:text-xl"
+          role="button"
+          tabIndex={0}
+        >
+          {getName(result, locale)}
+        </li>
+      );
+    });
 
   return (
     <div className="w-full max-h-full mt-2 overflow-y-auto bg-white rounded-lg shadow-lg dark:bg-gray-800">
       <ul className="divide-y divide-gray-300 dark:divide-gray-700">
         {results.reciters.length > 0 &&
-          renderResults(results.reciters, "various-recitations")}
+          renderResults(results.reciters, "reciter")}
         {results.recitations.length > 0 &&
-          renderResults(results.recitations, "frequent-recitations")}
+          renderResults(results.recitations, "recitation")}
         {results.surahs.length > 0 && renderResults(results.surahs, "surah")}
       </ul>
     </div>
