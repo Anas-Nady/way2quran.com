dc4f1dfc5b806f0db21c19aeb2d602ab447d4984
Anas Nady <eng.anas.nady@gmail.com>
2024-12-04 21:55:03 +0200

    refactor: remove `SearchInput` component from SurahList Page

--- client/src/app/[locale]/(content)/surahs/page.tsx
+++ client/src/app/[locale]/(content)/surahs/page.tsx
@@ -4,7 +4,6 @@ import { getTranslations } from "next-intl/server";
 import PageHeading from "@/components/ui/PageHeading";
 import SurahsList from "@/components/Surah/SurahsList";
 import { useTranslations } from "next-intl";
-import SearchInput from "@/components/ui/SearchInput";
 
 export async function generateMetadata({ params: { locale } }: PageParams) {
   const t = await getTranslations();
@@ -21,9 +20,6 @@ const SurahListPage: React.FC<PageParams> = ({ params: { locale } }) => {
     <div className="container p-3 mx-auto my-3 bg-white border shadow-xl dark:bg-slate-800 border-1 border-slate-200 dark:border-gray-600 sm:max-w-screen-2xl">
       <div className="flex flex-col gap-5">
         <PageHeading text={t("quranicSurahs")} />
-        <div className="mx-auto">
-          <SearchInput />
-        </div>
         <SurahsList locale={locale} />
       </div>
     </div>
