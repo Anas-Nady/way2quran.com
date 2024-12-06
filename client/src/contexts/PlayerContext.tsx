f71a33efe32054b11f1a3324bcb98c9ae3493bb2
Anas Nady <eng.anas.nady@gmail.com>
2024-12-06 20:16:25 +0200

    Refactored `handleAudioEnded` by removing unused `closeAudio` calls.

--- client/src/contexts/PlayerContext.tsx
+++ client/src/contexts/PlayerContext.tsx
@@ -65,7 +65,6 @@ const PlayerProvider: React.FC<PlayerProviderProps> = ({
       playerState.surahs[playerState.surahs.length - 1].number ===
         prevSurah.number
     ) {
-      closeAudio();
       return;
     }
 
@@ -73,7 +72,6 @@ const PlayerProvider: React.FC<PlayerProviderProps> = ({
       playerState.surahs[playerState.surahs.indexOf(prevSurah) + 1];
 
     if (!nextSurah) {
-      closeAudio();
       return;
     }
 
