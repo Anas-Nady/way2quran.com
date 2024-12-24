c108122179f6b1f1bffc568f5845e17fe7602ad3
Anas Nady <eng.anas.nady@gmail.com>
2024-12-24 09:32:24 +0200

    Refactor `getErrorMessage`:  to handle generic Error instances alongside AxiosError

--- client/src/helpers/getErrorMessage.ts
+++ client/src/helpers/getErrorMessage.ts
@@ -1,4 +1,10 @@
+import { AxiosError } from "axios";
+
 const getErrorMessage = (error: unknown) => {
+  if (error instanceof AxiosError) {
+    return error.response?.data?.message || error.message;
+  }
+
   if (error instanceof Error) {
     return error.message;
   }
