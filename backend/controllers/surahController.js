9e44bd63ef4ea07bc537df81db4cedbfce9792b5
Anas Nady <eng.anas.nady@gmail.com>
2025-01-28 02:11:39 +0200

    Deleted unused getSurahWithReciter from surahController.js and surahRoute.js

--- backend/controllers/surahController.js
+++ backend/controllers/surahController.js
@@ -1,8 +1,6 @@
 const AppError = require("../utils/appError.js");
 const asyncHandler = require("express-async-handler");
 const Surah = require("./../models/surahModel.js");
-const Reciter = require("./../models/reciterModel.js");
-const Recitation = require("./../models/recitationModel.js");
 
 exports.getSurahInfo = asyncHandler(async (req, res, next) => {
   const slug = req.params.slug;
@@ -44,106 +42,5 @@ exports.getAllSurahs = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     status: "success",
     surahs,
-  });
-});
-
-exports.getSurahWithReciter = asyncHandler(async (req, res, next) => {
-  const surahSlug = req.params.surahSlug;
-  const reciterSlug = req.params.reciterSlug;
-  const recitationSlug = req.params.recitationSlug;
-
-  const isRecitationExists = await Recitation.findOne({ slug: recitationSlug });
-  if (!isRecitationExists) {
-    return next(
-      new AppError(`Recitation: ${recitationSlug} is not found`, 404)
-    );
-  }
-
-  const isSurahExists = await Surah.findOne({ slug: surahSlug });
-  if (!isSurahExists) {
-    return next(new AppError(`Surah: ${surahSlug} is not found`, 404));
-  }
-
-  const filter = {
-    slug: reciterSlug,
-    "recitations.recitationInfo": isRecitationExists._id,
-    "recitations.audioFiles.surahInfo": isSurahExists._id,
-  };
-
-  const reciter = await Reciter.findOne(filter);
-  if (!reciter) {
-    return next(
-      new AppError(
-        `this resource: /${reciterSlug}/${recitationSlug}/${surahSlug} is not found`,
-        404
-      )
-    );
-  }
-
-  const surahInfo = { ...isSurahExists.toObject() };
-
-  const recitation = reciter.recitations.find(
-    (rec) => rec.recitationInfo.toString() === isRecitationExists._id.toString()
-  );
-
-  if (!recitation) {
-    return next(
-      new AppError(
-        `this resource: /${reciterSlug}/${recitationSlug}/${surahSlug} is not found`,
-        404
-      )
-    );
-  }
-
-  let audioFile;
-  let nextSurahNumber;
-  let previousSurahNumber;
-
-  for (let i = 0; i < recitation.audioFiles.length; i++) {
-    if (recitation.audioFiles[i].surahNumber == isSurahExists.number) {
-      audioFile = recitation.audioFiles[i];
-      nextSurahNumber = recitation.audioFiles[i + 1]?.surahNumber;
-      previousSurahNumber = recitation.audioFiles[i - 1]?.surahNumber;
-      break;
-    }
-  }
-
-  let nextSurah;
-  let previousSurah;
-
-  if (nextSurahNumber) {
-    nextSurah = await Surah.findOne({ number: nextSurahNumber }).select(
-      "arabicName englishName slug number"
-    );
-  }
-
-  if (previousSurahNumber) {
-    previousSurah = await Surah.findOne({ number: previousSurahNumber }).select(
-      "arabicName englishName slug number"
-    );
-  }
-
-  if (!audioFile) {
-    return next(new AppError("No audio file found for this surah", 404));
-  }
-
-  surahInfo.url = audioFile.url;
-  surahInfo.downloadUrl = audioFile.downloadUrl;
-
-  res.status(200).json({
-    status: "success",
-    surah: surahInfo,
-    nextSurah,
-    previousSurah,
-    recitation: {
-      arabicName: isRecitationExists.arabicName,
-      englishName: isRecitationExists.englishName,
-    },
-    reciter: {
-      arabicName: reciter.arabicName,
-      englishName: reciter.englishName,
-      slug: reciter.slug,
-      photo: reciter.photo,
-    },
   });
 });
--- backend/routes/surahRoute.js
+++ backend/routes/surahRoute.js
@@ -5,8 +5,5 @@ const surahController = require("./../controllers/surahController");
 router.route("/").get(surahController.getAllSurahs);
 
 router.route("/:slug").get(surahController.getSurahInfo);
-router
-  .route("/:reciterSlug/:recitationSlug/:surahSlug")
-  .get(surahController.getSurahWithReciter);
 
 module.exports = router;
