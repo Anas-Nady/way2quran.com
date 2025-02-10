86550dd8499231d7fc1e871c59c93f4b0e262dad
Anas Nady <eng.anas.nady@gmail.com>
2025-02-10 08:12:34 +0200

    Enhance GCS file deletion: Add existence check and improved error handling

--- server/utils/deleteFileFromGCS.ts
+++ server/utils/deleteFileFromGCS.ts
@@ -1,12 +1,45 @@
 import { bucketName, storage } from "../config/googleStorage";
+import AppError from "./appError";
 
 export const deleteFilesFromGCS = async (prefix: string) => {
-  const [files] = await storage.bucket(bucketName).getFiles({ prefix });
-  if (files.length) {
-    await Promise.all(files.map((file) => file.delete()));
+  try {
+    const [files] = await storage.bucket(bucketName).getFiles({ prefix });
+
+    if (files.length) {
+      await Promise.all(
+        files.map(async (file) => {
+          try {
+            const [exists] = await file.exists();
+            if (exists) {
+              await file.delete();
+            }
+          } catch (error) {
+            throw new AppError(
+              `Error deleting file ${file.name}: ${JSON.stringify(error)}`,
+              500
+            );
+          }
+        })
+      );
+    }
+  } catch (error) {
+    throw new AppError(`Error fetching files with prefix ${prefix}:`, 500);
   }
 };
 
 export const deleteFileFromGCS = async (filePath: string) => {
-  await storage.bucket(bucketName).file(filePath)?.delete();
+  try {
+    const file = storage.bucket(bucketName).file(filePath);
+
+    // Check if the file exists
+    const [exists] = await file.exists();
+    if (exists) {
+      await file.delete();
+    }
+  } catch (error) {
+    throw new AppError(
+      `Error deleting file ${filePath}: ${JSON.stringify(error)}`,
+      500
+    );
+  }
 };
