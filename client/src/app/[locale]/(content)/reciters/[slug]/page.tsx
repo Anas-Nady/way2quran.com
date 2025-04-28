cd6f0097fdaf0c96661b71907dc87882905f1fe0
Anas Nady <eng.anas.nady@gmail.com>
2025-04-28 08:11:10 +0300

    Remove `generateStaticParams` function from the reciters' page

--- client/src/app/[locale]/(content)/reciters/[slug]/page.tsx
+++ client/src/app/[locale]/(content)/reciters/[slug]/page.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import {
-  getReciterDetails,
-  getReciterInfo,
-  listAllReciters,
-} from "@/actions/reciters";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { getReciterDetails, getReciterInfo } from "@/actions/reciters";
 import getSocialMediaPhoto from "@/helpers/getSocialMediaPhoto";
 import shareMetadata from "../../_shareMetadata";
 import getName from "@/helpers/getNameForCurrentLang";
 import ErrorAlert from "@/components/common/ErrorAlert";
 import ReciterDetails from "@/components/Reciter/ReciterDetails";
-import { RECITATIONS } from "@/constants/Recitations";
 
 interface ReciterPageProps {
   params: { slug: string; locale: "ar" | "en" };
@@ -36,34 +30,9 @@ export async function generateMetadata({
   });
 }
 
-export async function generateStaticParams() {
-  const paths = [];
-
-  for (const recitation of RECITATIONS) {
-    const recitersRes = await listAllReciters({
-      recitationSlug: recitation.slug,
-      pageSize: 9000,
-    });
-
-    const recitersData = await recitersRes.json();
-    const reciters = recitersData.reciters;
-
-    // Generate paths
-    for (const reciter of reciters) {
-      paths.push({
-        slug: reciter.slug,
-      });
-    }
-  }
-
-  return paths;
-}
-
 const ReciterPage: React.FC<ReciterPageProps> = async ({
   params: { locale, slug },
 }) => {
-  unstable_setRequestLocale(locale);
-
   const res = await getReciterDetails({
     reciterSlug: slug,
     increaseViews: true,
